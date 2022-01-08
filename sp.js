5d6b00c10a74d2f78606d5947d59e89df19624f0
serhat <srhtdlny@gmail.com>
2022-01-08 22:45:48 +0300

    prevent to click repeatedly

--- sp.js
+++ sp.js
@@ -3,10 +3,16 @@ var voices = synth.getVoices();
 
 var inputForm = document.querySelector("form");
 var inputTxt = document.querySelector("textarea");
+var inputBtn = document.querySelector("input");
 
 inputForm.onsubmit = function (event) {
+  inputBtn.setAttribute("disabled", "disabled");
   event.preventDefault();
   var utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+  utterThis.lang = "tr-TR";
   synth.speak(utterThis);
+  utterThis.addEventListener("end", function (event) {
+    inputBtn.removeAttribute("disabled");
+  });
   inputTxt.blur();
 };
